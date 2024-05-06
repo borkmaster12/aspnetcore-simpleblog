3d4be840aa5d3bdbe8331a311d113d06989bccc4
borkmeister <borkmaster12@gmail.com>
2024-05-05 21:54:33 -0500

    Require auth for create blog

--- SimpleBlog/Pages/Blogs/Create.cshtml.cs
+++ SimpleBlog/Pages/Blogs/Create.cshtml.cs
@@ -1,10 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleBlog.Models;
 
 namespace SimpleBlog.Pages.Blogs
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly SimpleBlog.Data.SimpleBlogContext _context;
