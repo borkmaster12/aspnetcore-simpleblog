c1d21c8b41f1e340eae404b28e27bea2afad4ce8
borkmeister <borkmaster12@gmail.com>
2024-05-05 21:23:38 -0500

    Add Blog model

---
+++ SimpleBlog/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = default!;
+
+        [Required]
+        public string Content { get; set; } = default!;
+
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? LastUpdatedDate { get; set; }
+    }
+}
